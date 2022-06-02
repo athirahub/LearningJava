37451daf138bb1714823dab111de75a202d244b9
athirahub <athirahub@example.com>
2022-06-01 21:04:25 -0700

    Sumnumbersedited

--- src/assignmentusingwhile/SumNumbers.java
+++ src/assignmentusingwhile/SumNumbers.java
@@ -6,18 +6,19 @@ public class SumNumbers {
 
 	public static void main(String[] args) {
 		double num = 0;// num is the number user wants to enter//
-		double count = 0;// it is the sum of numbers entered, initially will be 0//
+		double count = 0;// it is the count of numbers entered, initially will be 0//
 		boolean isUserEntering = true;// variable to check whether user wants to continue entering numbers//
 		Scanner sc = new Scanner(System.in);
 		do {
 			System.out.println("Enter the number ");
 			System.out.println("Does user wants to continue entering number:True or False");
 			num = sc.nextDouble();
-			isUserEntering = sc.nextBoolean();
-			count = count + num;
+			isUserEntering = sc.nextBoolean();// reassigning value with input obtained from console//
+			count = count + 1;
+
 		} while (isUserEntering);
 
 		System.out.println("The count of numbers entered is :" + count);
-	}
 
+	}
 }
